03f0d497513b9e1e1bf05257f019cde4abef69fd
Eric Boutueil <e.boutueil@gmail.com>
2018-02-14 17:19:55 +0100

    models index show new create done & crash tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'restaurants/index'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'restaurants/show'
+
+  # # READ
+  # # READ ALL
+  get "restaurants", to: "restaurants#index"
+
+  #  # CREATE
+  # # Permet d'afficher le formulaire à remplir
+  get "restaurants/new", to: "restaurants#new"
+
+  # # Permet de soumettre les champs remplis par l'user
+  post "restaurants", to: "restaurants#create"
+
+  # # READ ONE
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # # UPDATE
+  # # Donner le formulaire à modifier
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit
+  # # Soummetre les modifs et enregistrer
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # # DELETE
+  # delete "restaurants/:id", to: "restaurants#destroy"
 end
